49a8af53ad20e8a9300ca5dbb01f1a07cdb0b1ae
Rakesh Nagilla <nagillarakeshreddy@gmail.com>
2014-11-24 16:00:01 -0500

    add dev key

--- app/models/video_search.rb
+++ app/models/video_search.rb
@@ -2,13 +2,13 @@ class VideoSearch
 	require 'youtube_it'
 
 	def self.videos
-		developer_key = ''
+		developer_key = ENV['DEV_KEY']
 		client = YouTubeIt::Client.new(:dev_key => developer_key)
 		client.videos_by(:most_viewed, :time => :today, :page => 1, :per_page => 20).videos
 	end
 
 	def self.india_videos
-		developer_key = ''
+		developer_key = ENV['DEV_KEY']
 		client = YouTubeIt::Client.new(:dev_key => developer_key)
 		client.videos_by(:most_viewed,  :region => "IN").videos
 	end
